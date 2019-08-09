9133835588ae4821c9021b30298888788f641fc6
amartincastro <castroarielm@gmail.com>
2019-08-09 10:20:44 -0700

    set up greeting functions

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+type englishBot struct{}
+type spanishBot struct{}
+
 func main() {
 
+}
+
+func (eb englishBot) getGreeting() string {
+	// VERY custom logic for generating an english greeting
+	return "Hi there!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
 }
